117e237249e416ac1eef5965c5cd05b0037bf8fd
mithileshtarkar <mithilesh.tarkar@ves.ac.in>
2020-08-31 12:38:18 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React from 'react';
+import {Pie} from 'react-chartjs-2';
 import logo from './logo.svg';
 import './App.css';
+const data = {
+	labels: [
+		'Red',
+		'Blue',
+		'Yellow'
+	],
+	datasets: [{
+		data: [300, 50, 100],
+		backgroundColor: [
+		'#FF6384',
+		'#36A2EB',
+		'#FFCE56'
+		],
+		hoverBackgroundColor: [
+		'#FF6384',
+		'#36A2EB',
+		'#FFCE56'
+		]
+	}]
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h2>Pie Example</h2>
+        <Pie data={data} />
+      </div>
     </div>
   );
 }
